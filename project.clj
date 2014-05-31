49d22d0e8bda11b7ab13801fb7411d9895d69eb8
Tobias Quinn <tobias@tobiasquinn.com>
2014-05-31 19:23:53 +0100

    moved to orientdb 1.6.6

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"sonatype" "https://oss.sonatype.org/content/groups/public/"}
   :plugins [[codox "0.6.1"]]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.orientechnologies/orient-commons "1.4.0"]
-                 [com.orientechnologies/orientdb-client "1.4.0"]
-                 [com.orientechnologies/orientdb-core "1.4.0"]
-                 [com.tinkerpop.blueprints/blueprints-orient-graph "2.4.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.orientechnologies/orient-commons "1.6.6"]
+                 [com.orientechnologies/orientdb-client "1.6.6"]
+                 [com.orientechnologies/orientdb-core "1.6.6"]
+                 [com.tinkerpop.blueprints/blueprints-orient-graph "2.4.0"]
                  [blueprints.clj "0.1.0-SNAPSHOT"]])
